42304035a854ee949bf8e10fe3717ea5df1b3c27
Thach Steven <thachsteven.marketing@gmail.com>
2021-10-11 12:07:27 +0700

    first commit

---
+++ src/components/Aos.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export default function Aos() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
+
+  return (
+    <div>
+      <div className="item" data-aos="fade-up">
+        1
+      </div>
+      <div className="item" data-aos="fade-down">
+        2
+      </div>
+      <div className="item" data-aos="fade-right">
+        3
+      </div>
+      <div className="item" data-aos="fade-left">
+        4
+      </div>
+      <div className="item" data-aos="zoom-in">
+        5
+      </div>
+      <div className="item" data-aos="zoom-out">
+        6
+      </div>
+      <div className="item" data-aos="slide-up">
+        7
+      </div>
+      <div className="item" data-aos="flip-up">
+        8
+      </div>
+      <div className="item" data-aos="flip-down">
+        9
+      </div>
+      <div className="item" data-aos="flip-right">
+        10
+      </div>
+      <div className="item" data-aos="flip-left">
+        11
+      </div>
+      <div className="item" data-aos="fade-up">
+        12
+      </div>
+      <div className="item" data-aos="fade-down">
+        13
+      </div>
+      <div className="item" data-aos="fade-right">
+        14
+      </div>
+    </div>
+  );
+}
